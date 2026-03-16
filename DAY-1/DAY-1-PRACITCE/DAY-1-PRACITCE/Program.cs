0d68e560b67e43a57fd09c0fa6bbc0b231bdf10d
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-16 12:40:29 +0530

    Day 1 DSA: Implement FindMax, CountEven and ReverseArray methods

---
+++ DAY-1/DAY-1-PRACITCE/DAY-1-PRACITCE/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_1_PRACITCE
+{
+    internal class Program
+    {
+        public static int  FindMax(int[] array)
+        {
+            int max=array[0];
+            for(int i = 1; i < array.Length; i++)
+            {
+                if(array[i] > max)
+                {
+                    max = array[i];
+                }
+
+            }
+            return max;
+        }
+
+        public static int EvenNo(int[] array)
+        {
+            int even_Number = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    even_Number++;
+                }
+            }
+            return even_Number;
+        }
+
+        public static int[] Reverse(int[] array)
+        {
+            for(int i = 0; i < array.Length/2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - 1-i];
+                array[array.Length - 1-i] = temp;
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
