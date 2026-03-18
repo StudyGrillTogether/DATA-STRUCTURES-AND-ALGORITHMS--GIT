132b8489847112196b5c5de2da01e0dc07a7f865
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-18 17:05:06 +0530

    -added a method to count no.of duplicates in the array

--- DAY-3/DAY-3-GIT-EXERCISE/DAY-3-GIT-EXERCISE/Program.cs
+++ DAY-3/DAY-3-GIT-EXERCISE/DAY-3-GIT-EXERCISE/Program.cs
@@ -37,6 +37,22 @@ namespace DAY_3_GIT_EXERCISE
             }
             return second;
         }
+        static int CountDuplicates(int[] array)
+        {
+            int duplicates = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j=i+1;j<array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        duplicates++;
+                        break;
+                    }
+                }
+            }
+            return duplicates;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the no.of elemetns in an array: ");
