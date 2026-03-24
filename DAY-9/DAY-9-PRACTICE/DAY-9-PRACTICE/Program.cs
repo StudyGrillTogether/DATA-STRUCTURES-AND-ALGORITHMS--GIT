a24c66d250b7667c0db6ab9f790d59c1eacff2fd
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-24 15:38:11 +0530

    Day 9 DSA: Sorting - BubbleSort, SelectionSort, InsertionSor

--- DAY-9/DAY-9-PRACTICE/DAY-9-PRACTICE/Program.cs
+++ DAY-9/DAY-9-PRACTICE/DAY-9-PRACTICE/Program.cs
@@ -34,7 +34,7 @@ namespace DAY_9_PRACTICE
             for (int i = 0; i < array.Length - 1; i++)
             {
                 int minIndex = i;
-                for (int j = i +1; j < array.Length - 1 - i; j++)
+                for (int j = i +1; j < array.Length; j++)
                 {
                     if (array[j] < array[minIndex])
                     {
@@ -48,20 +48,40 @@ namespace DAY_9_PRACTICE
             }
             return array;
         }
+
+        public static int[] InsertionSort(int[] array)
+        {
+            for(int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while(j>=0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+            return array;
+        }
          static void Main(string[] args)
         {
-            int[] newArray = { 5, 3, 8, 2, 1 };
-            int[] n=new int[newArray.Length];
-            n=BubbleSort(newArray);
-            for(int i = 0; i < n.Length; i++)
-            {
-                Console.WriteLine(n[i]);
-            }
-            n = SelectionSort(newArray);
-            for(int i = 0; i < n.Length; i++)
-            {
-                Console.WriteLine(n[i]);
-            }
+            int[] arr1 = { 5, 3, 8, 2, 1 };
+            int[] arr2 = { 5, 3, 8, 2, 1 };
+            int[] arr3 = { 5, 3, 8, 2, 1 };
+
+            BubbleSort(arr1);
+            SelectionSort(arr2);
+            InsertionSort(arr3);
+
+            Console.Write("Bubble Sort:    ");
+            foreach (int i in arr1) Console.Write(i + " ");
+
+            Console.Write("\nSelection Sort: ");
+            foreach (int i in arr2) Console.Write(i + " ");
+
+            Console.Write("\nInsertion Sort: ");
+            foreach (int i in arr3) Console.Write(i + " ");
         }
     }
 }
