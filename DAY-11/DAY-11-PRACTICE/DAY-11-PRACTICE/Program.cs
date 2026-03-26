fd680f5252323fb097937b57e83540b6e5373ba9
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-26 11:36:29 +0530

    -created class Node for Buinary tree and Binary tree which will store these nodes

---
+++ DAY-11/DAY-11-PRACTICE/DAY-11-PRACTICE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_11_PRACTICE
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+
+            public Node(int data)
+            {
+                this.data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        class BinaryTree
+        {
+            Node root; //points to root node
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
