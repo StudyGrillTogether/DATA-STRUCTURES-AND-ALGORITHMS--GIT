f28bd2ebc1ab7696afb873f68aab2650ceae22c2
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-26 11:59:56 +0530

    Day 11 DSA: Binary Tree - Node class, Inorder, Preorder, Postorder traversals

--- DAY-11/DAY-11-PRACTICE/DAY-11-PRACTICE/Program.cs
+++ DAY-11/DAY-11-PRACTICE/DAY-11-PRACTICE/Program.cs
@@ -24,30 +24,54 @@ namespace DAY_11_PRACTICE
 
         class BinaryTree
         {
-            Node root; //points to root node
+            public Node root; //points to root node
 
             public void InOrder(Node node)
             {
                 if (node == null) return;
                 InOrder(node.left); //go left
-                Console.WriteLine(node.data); //print
+                Console.Write(node.data+" "); //print
                 InOrder(node.right);//go right
             }
 
             public void PreOrder(Node node)
             {
                 if (node == null) return;
-                Console.WriteLine(node.data);
+                Console.Write(node.data+" ");
                 PreOrder(node.left);
                 PreOrder(node.right);
             }
 
-            public void PostOrder(Node node) {
+            public void PostOrder(Node node)
+            {
+                if (node == null) return;
+                PostOrder(node.left);
+                PostOrder(node.right);
+                Console.Write(node.data+" ");
+            }
         }
 
         
         static void Main(string[] args)
         {
+            BinaryTree tree= new BinaryTree();
+            tree.root=new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+
+            Console.WriteLine("Inorder:");
+            tree.InOrder(tree.root);
+
+            Console.WriteLine();
+            Console.WriteLine("Preorder:");
+            tree.PreOrder(tree.root);
+
+            Console.WriteLine() ;
+            Console.WriteLine("Postorder:");
+            tree.PostOrder(tree.root);
+
         }
     }
 }
