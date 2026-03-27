a6e268b19e3b74b1d716a43a0f36d64d250866ff
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-28 03:43:43 +0530

    Day 12 DSA: Binary Search Tree - Insert, Search and Hash Tables - Dictionary

---
+++ DAY-12/DAY-12-PRACTICE/DAY-12-PRACTICE/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_12_PRACTICE
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+
+            public Node(int data)
+            {
+                this.data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        class BinaryTree
+        {
+            public Node root;
+
+            public void Insert(int value)
+            {
+                root=InsertRecursive(root, value);
+            }
+
+            private Node InsertRecursive(Node node,int value)
+            {
+                if (node == null)
+                {
+                    return new Node(value);
+                }
+                else if (value < node.data)
+                {
+                   node.left=InsertRecursive(node.left,value); //GOING LEFT
+                    
+                }
+                else
+                {
+                    node.right=InsertRecursive(node.right,value);
+                }
+
+                return node;
+            }
+
+            public bool BSTSearch(int value)
+            {
+                return SearchRecursive(root, value);
+            }
+
+            private bool SearchRecursive(Node node,int value)
+            {
+                if (node == null)
+                {
+                    return false;
+                }
+                else if (value == node.data)
+                {
+                    return true;
+                }
+                else  if(value<node.data)
+                {
+                   return SearchRecursive(node.left,value);
+                }
+                else
+                {
+                    return SearchRecursive(node.right,value);
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Insert(5);
+            tree.Insert(3);
+            tree.Insert(8);
+            tree.Insert(2);
+            tree.Insert(4);
+            tree.Insert(6);
+            tree.Insert(9);
+
+            Console.WriteLine(tree.BSTSearch(6));
+            Console.WriteLine(tree.BSTSearch(7));
+
+            Dictionary<string,int> dict = new Dictionary<string,int>();
+            dict["john"] = 25;
+            dict["jane"] = 30;
+
+            //search
+            Console.WriteLine(dict["john"]);
+
+            //check if key exisxts
+
+            Console.WriteLine(dict.ContainsKey("john")); //true
+
+            //delete
+            dict.Remove("john");
+
+            foreach(var i in dict.Keys)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+        }
+    }
+}
---
+++ DAY-12/DAY-12-PRACTICE/DAY-12-PRACTICE/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DAY-12-PRACTICE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DAY-12-PRACTICE")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69a8d409-822e-4bea-826b-401d177ca776")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
